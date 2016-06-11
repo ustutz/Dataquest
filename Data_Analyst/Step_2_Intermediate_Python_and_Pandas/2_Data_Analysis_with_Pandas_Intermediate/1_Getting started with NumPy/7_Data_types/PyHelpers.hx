7102e4ce99ec4908f0c3343b520f57e64889752b
Urs Stutz <gitter@ls3d.de>
2016-06-11 11:22:07 +0200

    PyHelpers.call fix for haxe 3.3

--- Data_Analyst/Step_2_Intermediate_Python_and_Pandas/2_Data_Analysis_with_Pandas_Intermediate/1_Getting started with NumPy/7_Data_types/PyHelpers.hx
+++ Data_Analyst/Step_2_Intermediate_Python_and_Pandas/2_Data_Analysis_with_Pandas_Intermediate/1_Getting started with NumPy/7_Data_types/PyHelpers.hx
@@ -22,32 +22,22 @@ class KwCall {
 
 	macro static public function call(func:ExprOf<haxe.Constraints.Function>, args:Array<Expr>):Expr {
 		var realArgs:Array<Expr> = [];
-		var argMap = new Map<String,Expr>();
+		var buildKwArgs:Array<Expr> = [macro var kwArgs = new python.Dict<String,Dynamic>()];
 		var kwAppeared = false;
 		for (e in args) switch (e) {
 			case macro $i{k} => $v:
 				kwAppeared = true;
-				argMap[k] = v;
+				buildKwArgs.push(macro kwArgs.set($v{k}, $v));
 			case _:
 				if (kwAppeared)
 					Context.error("Invalid expr. Should be in the form of `key => value`.", e.pos);
 				else
 					realArgs.push(e);
 		};
+		buildKwArgs.push(macro kwArgs);
+		realArgs.push(macro ($b{buildKwArgs}:python.KwArgs<Dynamic>));
 		switch (Context.typeof(func)) {
 			case TFun(args, ret):
-				for (a in args.slice(realArgs.length)) {
-					if (argMap.exists(a.name)) {
-						realArgs.push(argMap[a.name]);
-						argMap.remove(a.name);
-					} else {
-						break;
-					}
-				}
-				for (k in argMap.keys()) {
-					var v = argMap[k];
-					realArgs.push(macro python.Syntax.assign(python.Syntax.pythonCode($v{k}), $v));
-				}
 				return macro ($func:haxe.Constraints.Function)($a{realArgs});
 			case _:
 				Context.error("should be a TFun", func.pos);
